c5dfcb03bf70c562b242e026c87e17313ecb8cd4
Scott Bellware <sbellware@gmail.com>
2019-08-09 13:23:20 -0500

    Positional parameters are recorded

--- lib/invocation/controls/invocation.rb
+++ lib/invocation/controls/invocation.rb
@@ -6,9 +6,20 @@ class Invocation
         subject.some_method
       end
 
+      def self.positional_parameters(some_parameter, some_other_parameter)
+        subject = Subject::PositionalParameters.new
+        subject.some_method(some_parameter, some_other_parameter)
+      end
+
       module Subject
         class NoParameters
           def some_method
+            ::Invocation.build(binding)
+          end
+        end
+
+        class PositionalParameters
+          def some_method(some_parameter, some_other_parameter)
             ::Invocation.build(binding)
           end
         end
