2a0cdf1415a791c7a04a6c55cfea8304e1db733c
Nathan Ladd <nathanladd@gmail.com>
2024-01-21 17:31:26 -0600

    load_path.rb is corrected

--- load_path.rb
+++ load_path.rb
@@ -6,3 +6,5 @@ unless libraries_dir.nil?
   libraries_dir = File.expand_path(libraries_dir)
   $LOAD_PATH.unshift libraries_dir unless $LOAD_PATH.include?(libraries_dir)
 end
+
+require_relative 'gems/gems_init'
